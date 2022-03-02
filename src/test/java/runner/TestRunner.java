17aeb6660b188731dfd9257aba4177e476822bba
jumakhonov <aleemjumakhonov@yahoo.com>
2022-03-01 18:56:16 -0800

    first commit

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,26 @@
+package runner;
+
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import utilities.CucumberReportingConfig;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features", 
+glue = {"stepDefinitions"}, 
+monochrome = true,
+tags = "@regression",
+dryRun = false,
+strict = true,
+plugin = {"pretty", 
+		"html:target/site/cucumber-pretty", 
+		"json:target/cucumber.json"})
+public class TestRunner {
+
+	@AfterClass
+	public static void generateReport() {
+		CucumberReportingConfig.reportConfig();
+	}
+}
